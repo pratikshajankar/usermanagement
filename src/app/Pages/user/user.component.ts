ca48226985fe723b9f9c0c679f844f093382f599
pratikshajankar <164823486+pratikshajankar@users.noreply.github.com>
2025-02-01 09:38:41 +0530

    StylingComponent

--- src/app/Pages/user/user.component.ts
+++ src/app/Pages/user/user.component.ts
@@ -10,31 +10,55 @@ import { IUser } from 'src/app/core/modules/Interfaces/IUser';
 })
 export class UserComponent implements OnInit {
 
+  // Holds the search value
+  searchItem:string='';
+  // filtered list displayed in table
+  filteredUsers:any[]=[];
+
   // Creating a form object
   userForm:FormGroup=new FormGroup({});
 // creating user class object
 UserObj:User=new User();
 // Creating list of user interface
 UserList:IUser[]=[];
-// UserList: { userid: number; name: string; email: string; role:string }[] = [];
   constructor() { }
 
   ngOnInit(): void {
 
-    // this.UserList=[
-    //   { userid: 1, name: 'John Doe', email: 'john@example.com', role:'User' },
-    //     { userid: 2, name: 'Alice Johnson', email: 'alice@example.com', role:'Admin' },
-    //     { userid: 3, name: 'Mark Brown', email: 'mark@example.com', role:'User' }
-    // ];
+
+    // Initialize with full list
+    this.filteredUsers=[...this.UserList];
 
     // calling createForm()
     this.createForm();
 
     const oldData=localStorage.getItem("UserData");
+    this.UserList = oldData ? JSON.parse(oldData) : [];
     if(oldData!=null){
       const parseData=JSON.parse(oldData);
+      console.log('Loaded from localStorage:', this.UserList);
     }
 
+  }
+
+  // Function to filter user by name
+  filterUsers(){
+    this.filteredUsers=this.UserList.filter(user=>user.name.toLowerCase().includes(this.searchItem.toLowerCase())
+  );
+  }
+
+  // Method to save the role change for a user
+  onSaveRole(user:any){
+    // find the user in the list and update their role
+    const userIndex=this.UserList.findIndex(u=>u.userid===user.userid);
+    if(userIndex!==-1){
+      // update the role in the userlist
+      this.UserList[userIndex].role=user.role;
+
+      // Save the update list back to local storage
+      localStorage.setItem("UserData",JSON.stringify(this.UserList));
+      alert('Role for {user.name} has been updated to {user.role}');
+    }
   }
 
   // creatig form
@@ -52,14 +76,20 @@ UserList:IUser[]=[];
   onSave(){
     const oldData=localStorage.getItem("UserData");
     if(oldData!=null){
+      const newUser = this.userForm.value;
       const parseData=JSON.parse(oldData);
       this.UserList.unshift(this.userForm.value);
+
+      newUser.userid = parseData.length > 0 
+      ? parseData[parseData.length - 1].userid + 1 // Get last user's ID and increment
+      : 1; 
     }
     else{
       this.UserList.unshift(this.userForm.value);
     }
     localStorage.setItem("UserData",JSON.stringify(this.UserList));
     alert("User Added Successfully");
+    this.onReset();
   }
 
   // Updating information of user on clicking update button
@@ -70,8 +100,8 @@ UserList:IUser[]=[];
       record.email=this.userForm.controls['email'].value;
       record.role=this.userForm.controls['role'].value;
     }
-    localStorage.setItem("UserData",JSON.stringify(this.UserList));
     alert("User Updated Successfully");
+    this.onReset();
   }
 
   // open data in editable form
